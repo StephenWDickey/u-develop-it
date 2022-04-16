275bb24173b2c9450e45b3fc6551941a68dcbbc9
stevey d <stphndcky22@gmail.com>
2022-04-15 19:39:36 -0700

    placed MySQL queries into GET and POST requests

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 // we must import mysql2 module to connect to MySQL database
 const mysql = require('mysql2');
 
+// this module is in our POST request
+const inputCheck = require('./utils/inputCheck');
 
 ///////////////////////////////////////////
 
@@ -87,23 +89,58 @@ const db = mysql.createConnection (
 // if there are NO err, err becomes NULL
 // and we also obtain the database query response (rows)
 // this returns an array of objects, with each object being a row
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// now wrap all of this in a get request!
+// we name our endpoint as /api/candidates
+app.get('/api/candidates', (req, res) => {
 
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        // here we chain two methods to our response
+        // we state the status if there's an error
+        // and we use json to send an object with two properties
+        // err.message should describe the issue
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      // if no error we send an object with our data
+      // our data is called 'rows'
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
 //////////////////////////////////////////////
 
 
 // we create another query to the database, this time to retrieve only
 // one candidate
-db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
-});
+// this endpoint has a parameter of id, which can be any id
+app.get('/api/candidate/:id', (req, res) => {
 
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    // in our parameters array we specify that req.params object has
+    // a single element of id
+    const params = [req.params.id];
+
+    // the id that is specified at the endpoint will be queried to
+    // the database, we get a single row so we write 'row'
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      
+      res.json({
+        message: 'success',
+        data: row
+      });
+      
+    });
+  });
 
 ////////////////////////////////////////////////
 
@@ -115,24 +152,78 @@ db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
 // the 1 after the statement is a parameter that hardcodes a value
 // to use instead of ?
 // we can make this parameter an array with many values!
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+// delete() is an HTTP request method
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } 
+        // if there are no rows affected (meaning no deletions)
+        // then we will send object with message 'candidate not found'
+        else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } 
+        // if there IS affectedRows, then we will display those
+        else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
 
 
 ///////////////////////////////////////////////////////////
 
 
-// this query will create a candidate
-// we make a variable with our template literal for inserting data
-// again we are using placeholders for our values
+// we want this request to be sent to /api/candidate endpoint
+// this way it gets added to the candidate data
+// normally in the callback response we would put req.body
+// but here we are 'destructuring' the object to get the body property
+app.post('/api/candidate', ({ body }, res) => {
+    // this inputCheck function was provided to us
+    // it is in inputCheck.js
+    // we need to import this module at the top
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    // now if there are no errors we want to make a query to the db
+    // this query will create a candidate
+    // we make a variable with our template literal for inserting data
+    // again we are using placeholders for our values
+    // we create template literal for mysql commands
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+    // we make a variable with our params array
+    // this is an array of different values that we can use as params!
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    // pass our variable arguments in, put err and result in callback
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            // we display body data if successful
+            data: body
+        });
+    });
+});
+
+
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
 
-// we make a variable with our params array
-// this is an array of different values that we can use as params!
+
 const params = [1, 'Ronald', 'Firbank', 1];
 
 // now the actual query function
