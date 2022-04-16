0e4be8d6916a0297532d6b738da7f4e662dbff41
stevey d <stphndcky22@gmail.com>
2022-04-15 18:09:09 -0700

    imported mysql2 module, created connection to database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-// we import the module
+// we import the express module
 const express = require('express');
+
+// we must import mysql2 module to connect to MySQL database
+const mysql = require('mysql2');
 
 ///////////////////////////////////////////
 
@@ -32,6 +35,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+////////////////////////////////////////////////////
+
+// we use mysql2 to connect to MySQL database
+// we set equal to a variable named db
+const db = mysql.createConnection (
+
+    // we have an object here with the following properties
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'Splinter_6',
+        // select the database we're using
+        database: 'election'
+
+    },
+
+    console.log('Connected to the election database.')
+    
+);
+
+
 ////////////////////////////////////////////////
 
 
@@ -53,8 +79,23 @@ app.use(express.json());
 ///////////////////////////////////////////////
 
 
+// we create a query to our MySQL database!
+// our db object created above is using query() method
+// in the callback function we get the error (err) response
+// if there are NO err, err becomes NULL
+// and we also obtain the database query response (rows)
+// this returns an array of objects, with each object being a row
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+
+//////////////////////////////////////////////
+
+
 // we need to add error handling for bad responses (404s)
 // this response occurs when user uses incompatible endpoints
+// this is called a 'catchall' route
 app.use((req, res) => {
     res.status(404).end();
 });
