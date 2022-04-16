6b498e29b84490a92ee7bde4222dbe12eb82a8fc
stevey d <stphndcky22@gmail.com>
2022-04-15 18:46:11 -0700

    created queries for candidate data acquisition, candidate creation, candidate deletion

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 // we must import mysql2 module to connect to MySQL database
 const mysql = require('mysql2');
 
+
 ///////////////////////////////////////////
 
 
@@ -36,6 +37,7 @@ app.use(express.json());
 
 
 ////////////////////////////////////////////////////
+
 
 // we use mysql2 to connect to MySQL database
 // we set equal to a variable named db
@@ -93,12 +95,68 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
 //////////////////////////////////////////////
 
 
+// we create another query to the database, this time to retrieve only
+// one candidate
+db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+
+////////////////////////////////////////////////
+
+
+// we will create another query to delete a candidate from the db
+// the question mark ? indicates a placeholder
+// this is called a PREPARED STATEMENT, allows execution with
+// different values each time
+// the 1 after the statement is a parameter that hardcodes a value
+// to use instead of ?
+// we can make this parameter an array with many values!
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+
+///////////////////////////////////////////////////////////
+
+
+// this query will create a candidate
+// we make a variable with our template literal for inserting data
+// again we are using placeholders for our values
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
+
+// we make a variable with our params array
+// this is an array of different values that we can use as params!
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// now the actual query function
+// we pass our template literal for data insertion in as argument
+// followed by parameter array
+// and finally our callback function
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+
+//////////////////////////////////////////////////////////
+
+
 // we need to add error handling for bad responses (404s)
 // this response occurs when user uses incompatible endpoints
 // this is called a 'catchall' route
 app.use((req, res) => {
     res.status(404).end();
 });
+
 
 ///////////////////////////////////////////////
 
