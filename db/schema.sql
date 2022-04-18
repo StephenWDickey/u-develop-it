974ac7ee4419de6d0f309310e77dfaf888ef8290
stevey d <stphndcky22@gmail.com>
2022-04-18 13:40:08 -0700

    added voters table

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,7 @@
 -- then we create a new table with correct info
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,3 +24,12 @@ CREATE TABLE candidates (
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  -- we use DATETIME data type
+  -- we set the DEFAULT setting to CURRENT_TIMESTAMP
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
