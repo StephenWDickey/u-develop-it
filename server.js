fedfc76b7a8b2e11ffea6dd03d7305a2032a3392
stevey d <stphndcky22@gmail.com>
2022-04-18 12:38:56 -0700

    added GET and DELETE requests for parties endpoints, added PUT request to update candidate party_id

--- server.js
+++ server.js
@@ -160,6 +160,47 @@ app.get('/api/candidate/:id', (req, res) => {
 ////////////////////////////////////////////////
 
 
+
+// we create a get request for /parties endpoint
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+
+////////////////////////////////////////////////////
+
+
+
+// we create get request with 'id' parameter
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+
+//////////////////////////////////////////////////
+
+
 // we will create another query to delete a candidate from the db
 // the question mark ? indicates a placeholder
 // this is called a PREPARED STATEMENT, allows execution with
@@ -196,6 +237,72 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 
 ///////////////////////////////////////////////////////////
+
+
+
+// we add a delete request for /party/:id endpoint
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
+//////////////////////////////////////////////////////////////
+
+// the PUT request is better for updating
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+
+  // we will call our inputCheck() function to make sure
+  // that a party id has been provided
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  
+  // req.body to update body, req.params for endpoint
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
+//////////////////////////////////////////////////////////////
 
 
 // we want this request to be sent to /api/candidate endpoint
